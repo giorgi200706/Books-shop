af30e8dc46bf6745a4d1df27279dc779715604dd
giorgi200706 <giorgich31@gmail.com>
2022-05-31 17:06:55 +0400

    done

--- sql.py
+++ sql.py
@@ -4,7 +4,10 @@ con = sqlite3.connect('shop.db', check_same_thread=False)
 cur = con.cursor()
 
 #                                                                                                  GHS653VBA62
-# cur.execute('''insert into books values ("The Anubis Gates" , "Tim Powers" , 1983 , 12.99 ,       "3GV72B7CBV") ''')
+# cur.execute('''insert into books values ("The Anubis Gates" , "Tim Powers" , 1983 , 12.99 ,     "3GV72B7CBV") ''')
+
+# cur.execute('''create table users (name text , surname text , gmail text , password text)''')
+# cur.execute("INSERT INTO users (name , surname , gmail , password) VALUES (? , ? , ? , ?)" , (name , surname , gmail , password))
 
 # get data from books
 data = cur.execute("select * from books")
