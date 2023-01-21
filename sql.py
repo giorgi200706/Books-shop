2ca77376cd42bc2ecd440c79b66c8be24579b44a
giorgi200706 <giorgich31@gmail.com>
2023-01-21 22:46:49 +0400

    .

--- sql.py
+++ sql.py
@@ -6,8 +6,8 @@ cur = con.cursor()
 #                                                                                                  GHS653VBA62
 # cur.execute('''insert into books values ("The Anubis Gates" , "Tim Powers" , 1983 , 12.99 ,     "3GV72B7CBV") ''')
 
-# cur.execute('''create table users (name text , surname text , gmail text , password text)''')
-# cur.execute("INSERT INTO users (name , surname , gmail , password) VALUES (? , ? , ? , ?)" , (name , surname , gmail , password))
+cur.execute('''create table phones (company text , fullname text , memory text , color text , date text , size text , weight text , simcard text ,  screensize text , screenresolution text , operatingsystem text , chipset text , gpu tetx , internammemory text , ram text ,  additionalmemory text , maincamera text ,  video text , selfiecamera text , selfievideo text , bluthooth text , gps text , nfc text , fmradio text ,  port text ,sizemah text)''')
+# cur.execute('''create table usrs (name text , surname text , gmail text , password text)''')
 
 # get data from books
 data = cur.execute("select * from books")
